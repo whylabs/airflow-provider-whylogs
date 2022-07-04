b9c57e746af180a3c0fe4ccc24103ac7718d96db
murilommen <murilommen@gmail.com>
2022-07-04 18:12:24 -0400

    fix git ignore

---
+++ tests/operators/test_whylogs.py
@@ -0,0 +1,8 @@
+def test_summary_drift():
+    pass
+
+def test_constraints():
+    pass
+
+def test_multiple_constraints():
+    pass
---
+++ tests/operators/test_whylogs_helpers.py
@@ -0,0 +1,39 @@
+import os
+
+import pandas as pd
+
+from int_airflow.providers.whylogs.utils.whylogs import _read_csv, _read_parquet
+
+
+csv_data_path = os.path.join(os.getcwd(), "data/raw_data.csv")
+multiple_parquet_path = os.path.join(os.getcwd(), "data/parquet_example")
+
+
+# TODO read from s3
+
+# TODO write to local
+# TODO write to s3
+# TODO write to whylabs
+
+# TODO test dag execution
+# TODO test path with too many files (local, s3)
+
+# TODO define edge cases tests
+
+
+def test_read_csv_returns_dataframe():
+    dataframe = _read_csv(data_path=csv_data_path)
+    assert isinstance(dataframe, pd.DataFrame)
+    assert len(dataframe) > 0
+
+
+def test_read_csv_from_s3():
+    pass
+
+
+def test_read_parquet_reads_multi_files():
+    dataframe_generator = _read_parquet(multiple_parquet_path)
+    first_df = next(dataframe_generator)
+    second_df = next(dataframe_generator)
+    assert isinstance(first_df, pd.DataFrame) and isinstance(second_df, pd.DataFrame)
+    assert len(first_df > 0) and len(second_df > 0)
